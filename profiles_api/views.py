766f3549df3a5091106da45d394d1dc5e887416e
Idris <abiola282@gmail.com>
2020-09-18 15:30:30 -0400

    Configured for deployment

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -128,13 +128,6 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
         """Sets the user profile to the logged in user"""
         serializer.save(user_profile=self.request.user)
 
- 
-
-
-
-
-
-
 
 
 #from django.shortcuts import render
